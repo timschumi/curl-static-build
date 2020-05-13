e9d9683a713f88b28c7676e65bebd89c1b366a17
Tim Schumacher <timschumi@gmx.de>
2020-05-13 19:24:10 +0200

    build: Specify target manually

--- build.sh
+++ build.sh
@@ -8,6 +8,12 @@ if [ "$SCRIPTS_DIR" = "$WORKING_DIR" ]; then
     exit 1
 fi
 
+if [ -z "$1" ]; then
+    echo "error: No target given"
+    echo "usage: $0 <target>"
+    exit 1
+fi
+
 export CURL_SOURCE="$WORKING_DIR/curl"
 export CURL_BUILD="$WORKING_DIR/curl-build"
 export CURL_OUT="$SCRIPTS_DIR/curl"
@@ -20,30 +26,12 @@ export OPENSSL_OUT="$SCRIPTS_DIR/openssl"
 
 # Build zlib
 $SCRIPTS_DIR/zlib-sources.sh 1.2.11
-
-if [ -n "$1" ]; then
 $SCRIPTS_DIR/zlib-arch.sh "$1"
-else
-$SCRIPTS_DIR/zlib-arch.sh x64
-$SCRIPTS_DIR/zlib-arch.sh x86
-fi
 
 # Build openssl
 $SCRIPTS_DIR/openssl-sources.sh 1.1.1d
-
-if [ -n "$1" ]; then
 $SCRIPTS_DIR/openssl-arch.sh "$1"
-else
-$SCRIPTS_DIR/openssl-arch.sh x64
-$SCRIPTS_DIR/openssl-arch.sh x86
-fi
 
 # Build curl
 $SCRIPTS_DIR/curl-sources.sh 7.69.1
-
-if [ -n "$1" ]; then
 $SCRIPTS_DIR/curl-arch.sh "$1"
-else
-$SCRIPTS_DIR/curl-arch.sh x64
-$SCRIPTS_DIR/curl-arch.sh x86
-fi
