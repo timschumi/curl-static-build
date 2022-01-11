a2f339a20a7f2f904827c0da89a1f67268f640e0
Tim Schumacher <timschumi@gmx.de>
2022-01-11 21:02:35 +0100

    Disable the DST Root CA X3 if present during provisioning

--- vagrant-provision.sh
+++ vagrant-provision.sh
@@ -4,11 +4,15 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt update && apt -y upgrade
 
-apt -y install build-essential wget lsb-release pkg-config
+apt -y install build-essential wget lsb-release pkg-config ca-certificates
 
 if [ -n "$PROVISION_NEEDS_MINGW" ]; then
 	apt -y install mingw-w64
 fi
+
+# Disable DST Root CA X3 if present
+sed -i -e '/DST_Root_CA_X3/s/^!*/!/g' /etc/ca-certificates.conf
+update-ca-certificates
 
 # Create vagrant user if necessary
 if ! id -u vagrant; then
